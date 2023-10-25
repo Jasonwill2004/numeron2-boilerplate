f3f47c189633002c21a186a0d9cd69f08c897b63
Jasonwill2004 <jason.william@kalvium.community>
2023-10-25 18:15:59 +0530

    done

--- score.js
+++ score.js
@@ -1,1 +1,13 @@
 // Iteration 8: Making scoreboard functional
+document.getElementById("play-again-button").onclick = ()=>{
+    location.href = "game.html"
+}
+
+console.log(window.location.search)
+
+let params = new URLSearchParams(window.location.search)
+let score = params.get("score")
+console.log("score:",score)
+
+let scoreboard = document.getElementById("score-board")
+scoreboard.textContent = score
